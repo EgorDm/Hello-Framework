cc1492a8da92170efdafb448e345a2c956cf5110
Egor Dmitriev <egordmitriev2@gmail.com>
2017-07-06 22:44:43 +0200

    Initial commit

---
+++ framework/Hello.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egordm
+ * Date: 6-7-17
+ * Time: 22:31
+ */
+namespace Framework;
+
+class Hello {
+
+}
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egordm
+ * Date: 6-7-17
+ * Time: 21:37
+ */
+
+require __DIR__.'../vendor/autoload.php';
+
+echo 'hello world';
